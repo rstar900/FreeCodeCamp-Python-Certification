d76b06e65a1a3705d9bea3e9ca6f3b1e38ff5956
Rachit Garg <rachitgarg900@gmail.com>
2026-04-04 00:33:35 +0200

    Add y axis and o's visualization to create_spend_chart()

--- 17_Build_a_Budget_App/main.py
+++ 17_Build_a_Budget_App/main.py
@@ -46,8 +46,35 @@ class Category:
 
 def create_spend_chart(categories):
     result_str = 'Percentage spent by category'
-    # TODO: Figure out the rounding of the percentages to nearest 10 and the chart
-    return result_str
+    spent_dictionary = {} # Mapping b/w category:spent%
+    spent_total = 0.00 # Total withdrawals in all categories
+    for category in categories:
+        withdrawals = 0.00
+        # Extract the deposits
+        for dictionary in category.ledger:
+            if dictionary['amount'] < 0:
+                withdrawals += -dictionary['amount']
+        spent_dictionary[category.name] = withdrawals
+        spent_total += withdrawals
+    # Divide the spent percentages by total spent amount and round to nearest 10 for each category (round (perc, -1))
+    for category in categories:
+        spent_dictionary[category.name] = int(round((spent_dictionary[category.name] / spent_total) * 100, -1))
+    # Create the Y-Axis till 100
+    for y in range(100, -1,-10):
+        result_str += f'\n{y:>3}| '
+        # create bars for each category
+        for category in categories:
+            if spent_dictionary[category.name] >= y:
+                result_str += 'o'
+            else:
+                result_str += ' '
+            result_str += '  '             
+    # Create the horizontal lines
+    horizontal_lines = 1 + 3 * len(categories)
+    result_str += '\n' + 4 * ' ' + horizontal_lines * '-'
+    # TODO: Print category names vertically
+
+    return result_str + '\n' + str(spent_dictionary)
 
 # Test Code
 food = Category('Food')
@@ -56,4 +83,6 @@ food.withdraw(10.15, 'groceries')
 food.withdraw(15.89, 'restaurant and more food for dessert')
 clothing = Category('Clothing')
 food.transfer(50, clothing)
-print(food)   
+auto = Category('Auto')
+#print(food)
+print(create_spend_chart([food, clothing, auto]))   
